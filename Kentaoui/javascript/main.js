e621f3a05e76d34f34c4ed26f9d41cd795d47ea6
Ayoub CHAABAT <chaabat.02@gmail.com>
2023-10-20 17:27:54 +0100

    atay

---
+++ Kentaoui/javascript/main.js
@@ -0,0 +1,41 @@
+function imgSlide(imgSrc){
+    document.querySelector(".atay").src = imgSrc
+
+}
+
+function changeColors(color){
+    const shape = document.querySelector(".shape");
+    const button = document.querySelector(".learn-more");
+    const atay = document.querySelector("h2.product-header span");
+    shape.style.backgroundColor = color;
+    button.style.backgroundColor = color;
+    atay.style.color = color;
+}
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach( button =>{
+    button.addEventListener('click',()=>{
+        const faq = button.nextElementSibling;
+        const icon = button.children[1];
+
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+} )
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+var len = acc.length;
+for (i = 0; i < len; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
